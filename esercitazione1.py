0e5936d344218658eca863dab8d84c6343429ba8
DrisaTN <99647275+DrisaTN@users.noreply.github.com>
2022-02-15 12:36:49 +0100

    Update esercitazione1.py

--- esercitazione1.py
+++ esercitazione1.py
@@ -3,3 +3,6 @@ print("drisa ha la casa piena di cozza")
 print("drisa è passato di qua!")
 print("Quando scende la lava...")
 print("Brasiamo la padana")
+def mammt():
+  moio = input("inserisci il vero nome di quel bacchio di moio: ")
+  print("moio")
